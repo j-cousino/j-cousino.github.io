e0f750a6a120296d2b2d4c678adf1dee00acff2f
Jeff Cousino <jeff.cousino@soluforge.com>
2024-11-22 08:50:58 -0500

    Progeress

--- serve/src/main.rs
+++ serve/src/main.rs
@@ -1,13 +1,18 @@
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+use std::error::Error;
 
-    pretty_env_logger::init();
-    
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     website::main()?;
 
-    warp::serve(warp::fs::dir("site"))
-        .run(([0, 0, 0, 0], 8080))
-        .await;
+    let footer = format!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
 
-    anyhow::Ok(())
+    warpy::server::run(
+        format!("{}/../site", env!("CARGO_MANIFEST_DIR")),
+        [0, 0, 0, 0],
+        footer,
+        Some(8000),
+        false,
+    )
+    .await?;
+    Ok(())
 }
